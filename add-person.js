2a6118c1422b22a4fef7205b0b5b15c20ac37181
Tristan <tristanberezowski@gmail.com>
2019-02-27 00:16:15 +0000

    project finish with add person

---
+++ add-person.js
@@ -0,0 +1,23 @@
+const settings = require("./settings");
+const pg = require("knex") ({
+  client: 'pg',
+  connection: {
+    host     : settings.hostname,
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    ssl      : settings.ssl,
+    port     : settings.port
+  }
+  
+});
+
+pg('famous_people').insert({
+  first_name: process.argv[2],
+  last_name: process.argv[3],
+  birthdate: process.argv[4]
+})
+  .asCallback(function(err, rows) {
+    if (err) throw err;
+    pg.destroy();
+  });
